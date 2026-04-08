4400200c4012f779ce36a1df87dfb11ecc01af98
anikhil-at-363338988239 <sainikhil.abburi@vml.com>
2026-04-08 09:57:51 +0200

    initial commit

---
+++ app/main.py
@@ -0,0 +1,7 @@
+
+from fastapi import FastAPI
+from app.api.routes.health import router as health_router
+
+app = FastAPI(title="FastAPI ML Starter")
+
+app.include_router(health_router, prefix="/api/v1")
